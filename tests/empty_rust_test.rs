c1351b0c6754d57e8b6be1b8c48e2ad0eb2dd642
Razvan Statescu <razvan@statescu.net>
2022-11-15 20:04:09 +0200

    add tests

--- tests/empty_rust_test.rs
+++ tests/empty_rust_test.rs
@@ -1,7 +1,10 @@
-use elrond_wasm::types::{Address, ManagedAddress};
+use elrond_wasm::types::{Address, BigUint};
 use elrond_wasm_debug::{rust_biguint, testing_framework::*, DebugApi, managed_address};
 use staking::*;
 use std::time::{SystemTime, UNIX_EPOCH};
+use elrond_wasm::{
+    elrond_codec::multi_types::{OptionalValue},
+};
 
 const WASM_PATH: &'static str = "output/staking.wasm";
 pub const TOKEN_ID: &[u8] = b"STRM-df6f26";
@@ -26,11 +29,16 @@ where
 {
     let rust_zero = rust_biguint!(0u64);
     let mut blockchain_wrapper = BlockchainStateWrapper::new();
+
+    // Create a wallet for SC and assign 5M tokens
     let owner_address = blockchain_wrapper.create_user_account(&rust_zero);
     blockchain_wrapper.set_esdt_balance(&owner_address, TOKEN_ID, &rust_biguint!(5_000_000));
+
+    // Create 3 dummy wallets to interact with the protocol
     let first_user_address = blockchain_wrapper.create_user_account(&rust_zero);
     let second_user_address = blockchain_wrapper.create_user_account(&rust_zero);
     let third_user_address = blockchain_wrapper.create_user_account(&rust_zero);
+    
     let cf_wrapper = blockchain_wrapper.create_sc_account(
         &rust_zero,
         Some(&owner_address),
@@ -74,7 +82,11 @@ fn deploy_test() {
         .assert_ok();
 }
 
+/**
+ * Utility function to get current timestamp
+ */
 fn get_current_timestamp() -> u64 {
+    return 1668518731;
     let start = SystemTime::now();
     let since_the_epoch = start
         .duration_since(UNIX_EPOCH)
@@ -86,13 +98,18 @@ fn get_current_timestamp() -> u64 {
 fn create_stream_test() {
     let mut setup = setup_contract(staking::contract_obj);
     let b_wrapper = &mut setup.blockchain_wrapper;
+    let current_timestamp = get_current_timestamp();
+    b_wrapper.set_block_timestamp(current_timestamp);
+    let c_wrapper = &mut setup.contract_wrapper;
     let first_user = setup.first_user_address;
+    let owner_address  = setup.owner_address;
+    
 
-    // create a valid stream
+    // Create a valid stream of 3K tokens
     b_wrapper
         .execute_esdt_transfer(
-            &setup.owner_address,
-            &setup.contract_wrapper,
+            &owner_address,
+            c_wrapper,
             TOKEN_ID,
             0, 
             &rust_biguint!(3_000),
@@ -106,4 +123,175 @@ fn create_stream_test() {
             },
         )
         .assert_ok();
+
+    // Create an invalid stream of 0 tokens
+    b_wrapper
+    .execute_esdt_transfer(
+        &owner_address,
+        c_wrapper,
+        TOKEN_ID,
+        0, 
+        &rust_biguint!(0),
+        |sc| {
+            let current_timestamp = get_current_timestamp();
+             sc.create_stream(managed_address!(&first_user), current_timestamp + 60, current_timestamp + 60 * 60);
+        },
+    )
+    .assert_user_error("deposit is zero");
+
+    // Stream towards the SC
+    b_wrapper
+    .execute_esdt_transfer(
+        &owner_address,
+        c_wrapper,
+        TOKEN_ID,
+        0, 
+        &rust_biguint!(3_000),
+        |sc| {
+            let current_timestamp = get_current_timestamp();
+            sc.create_stream(managed_address!(c_wrapper.address_ref()), current_timestamp + 60, current_timestamp + 60 * 60);
+        },
+    )
+    .assert_user_error("stream to the current smart contract");
+
+    // Stream towards the caller
+    b_wrapper
+    .execute_esdt_transfer(
+        &owner_address,
+        c_wrapper,
+        TOKEN_ID,
+        0, 
+        &rust_biguint!(3_000),
+        |sc| {
+            let current_timestamp = get_current_timestamp();
+            sc.create_stream(managed_address!(&owner_address), current_timestamp + 60, current_timestamp + 60 * 60);
+        },
+    )
+    .assert_user_error("stream to the caller");
+
+    // Start time before current time
+    b_wrapper
+    .execute_esdt_transfer(
+        &owner_address,
+        c_wrapper,
+        TOKEN_ID,
+        0, 
+        &rust_biguint!(3_000),
+        |sc| {
+            let current_timestamp = get_current_timestamp();
+            sc.create_stream(managed_address!(&first_user), current_timestamp - 60, current_timestamp + 60 * 60);
+        },
+    )
+    .assert_user_error("start time before current time");
+
+     // End time before start time
+     b_wrapper
+     .execute_esdt_transfer(
+         &owner_address,
+         c_wrapper,
+         TOKEN_ID,
+         0, 
+         &rust_biguint!(3_000),
+         |sc| {
+             let current_timestamp = get_current_timestamp();
+             sc.create_stream(managed_address!(&first_user), current_timestamp + 60 * 60, current_timestamp + 60);
+         },
+     )
+     .assert_user_error("end time before the start time");
+}
+
+#[test]
+fn claim_from_stream_test() {
+    let mut setup = setup_contract(staking::contract_obj);
+    let b_wrapper = &mut setup.blockchain_wrapper;
+    let current_timestamp = get_current_timestamp();
+    b_wrapper.set_block_timestamp(current_timestamp);
+    let c_wrapper = &mut setup.contract_wrapper;
+    let first_user = setup.first_user_address;
+    let owner_address  = setup.owner_address;
+    
+
+    // Create a valid stream of 3K tokens
+    b_wrapper
+        .execute_esdt_transfer(
+            &owner_address,
+            c_wrapper,
+            TOKEN_ID,
+            0, 
+            &rust_biguint!(3_000),
+            |sc| {
+                let current_timestamp = get_current_timestamp();
+                 sc.create_stream(managed_address!(&first_user), current_timestamp + 60, current_timestamp + 60 * 3);
+            },
+        ).assert_ok();
+        
+
+        // Claim from stream wrong recipient
+        b_wrapper
+        .execute_tx(
+            &owner_address,
+            c_wrapper,
+            &rust_biguint!(0), 
+            |sc| {
+                sc.claim_from_stream(1, OptionalValue::None);
+            },
+        )
+        .assert_user_error("Only recipient can claim");
+
+          // Amount to claim is zero
+          b_wrapper
+          .execute_tx(
+              &first_user,
+              c_wrapper,
+              &rust_biguint!(0), 
+              |sc| {
+                  sc.claim_from_stream(1, OptionalValue::None);
+              },
+          )
+          .assert_user_error("amount is zero");
+
+          // Amount is bigger than streamed amount
+          b_wrapper
+          .execute_tx(
+              &first_user,
+              c_wrapper,
+              &rust_biguint!(0), 
+              |sc| {
+                  sc.claim_from_stream(1, OptionalValue::Some(BigUint::from(100u64)));
+              },
+          )
+          .assert_user_error("amount exceeds the available balance");
+
+          b_wrapper.set_block_timestamp(current_timestamp + 60 * 2);
+
+          // Claim 1.5K tokens
+          b_wrapper
+          .execute_tx(
+              &first_user,
+              c_wrapper,
+              &rust_biguint!(0), 
+              |sc| {
+                  sc.claim_from_stream(1, OptionalValue::None);
+              },
+          )
+          .assert_ok();
+
+          b_wrapper.check_esdt_balance(&first_user, TOKEN_ID, &rust_biguint!(1500));
+
+          b_wrapper.set_block_timestamp(current_timestamp + 60 * 5);
+
+          // Claim rest of the 1.5K tokens
+          b_wrapper
+          .execute_tx(
+              &first_user,
+              c_wrapper,
+              &rust_biguint!(0), 
+              |sc| {
+                  sc.claim_from_stream(1, OptionalValue::None);
+              },
+          )
+          .assert_ok();
+
+          b_wrapper.check_esdt_balance(&first_user, TOKEN_ID, &rust_biguint!(3000));
+
 }
