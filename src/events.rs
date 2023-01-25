270e94d8944456f12946a4c29bf37db7166c3b70
René Ďuriš <88586343+ReneDuris@users.noreply.github.com>
2023-01-25 17:26:46 +0100

    Update events.rs

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,7 @@
-elrond_wasm::imports!();
-elrond_wasm::derive_imports!();
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
 
-#[elrond_wasm::module]
+#[multiversx_sc::module]
 pub trait EventsModule {
     #[event("createStream")]
     fn create_stream_event(
